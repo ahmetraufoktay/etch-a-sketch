808acedab32990470bff752084800cf8ac736f94
ahmetraufoktay <68822176+ahmetraufoktay@users.noreply.github.com>
2024-01-29 16:08:19 +0300

    add limit for numberOfSqrPerSide

--- script.js
+++ script.js
@@ -1,11 +1,6 @@
 const grid = document.getElementById('grid');
 const button = document.getElementById('input');
 
-button.addEventListener('click', () => {
-    const numberOfSqrPerSide = prompt('Select number of squares per side: ');
-    createGrid(numberOfSqrPerSide);
-});
-
 function createGrid(x) {
     for (i=1;i<=x*x;i++) {
         const gridDiv = document.createElement('div');
@@ -20,3 +15,24 @@ function createGrid(x) {
 function colorDiv(e) {
     e.style.backgroundColor = 'blue';
 }
+
+createGrid(16);
+
+function input() {
+    let input = prompt('Select number of squares per side: ');
+    if (input >100) {
+        alert('Please prompt numbers less than 100')
+        input = '';
+    }
+    return input;
+}
+
+button.addEventListener('click', () => {
+    let numberOfSqrPerSide = input();
+    if (numberOfSqrPerSide !== null && numberOfSqrPerSide != '') {
+        while (grid.firstChild) {
+            grid.removeChild(grid.lastChild);
+        }
+        createGrid(numberOfSqrPerSide);
+    }
+});
