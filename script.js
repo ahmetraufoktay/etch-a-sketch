ded5ef00a23d0f0299e2559186acf015e4ae063f
ahmetraufoktay <68822176+ahmetraufoktay@users.noreply.github.com>
2024-01-29 06:04:02 +0300

    add click function and color function

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
 const grid = document.getElementById('grid');
-function createGrid() {
-    for (i=1;i<=256;i++) {
+const button = document.getElementById('input');
+
+button.addEventListener('click', () => {
+    const numberOfSqrPerSide = prompt('Select number of squares per side: ');
+    createGrid(numberOfSqrPerSide);
+});
+
+function createGrid(x) {
+    for (i=1;i<=x*x;i++) {
         const gridDiv = document.createElement('div');
         gridDiv.className = 'grid-element';
+        gridDiv.addEventListener("mouseover",()=> colorDiv(gridDiv));
+        gridDiv.style.height = `${640/x}px`;
+        gridDiv.style.width = `${640/x}px`
         grid.appendChild(gridDiv);
     }
 }
-createGrid();
+
+function colorDiv(e) {
+    e.style.backgroundColor = 'blue';
+}
